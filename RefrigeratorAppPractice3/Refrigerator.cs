5fb851bcd1e4e862c953cbda64e56f25923d9b10
HOSSAIN-ARMAN <hossainarman4389@gmail.com>
2019-10-16 23:44:08 +0600

    Refrigerator

---
+++ RefrigeratorAppPractice3/Refrigerator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    public class Refrigerator
+    {
+        private double maximumWeight;
+        private double item;
+        private double itemWeight;
+
+        public double MaxiMumWeight
+        {
+            set { this.maximumWeight = value; }
+            get { return this.maximumWeight; }
+            
+        }
+        public double Items
+        {
+            set { this.item = value; }
+            get { return this.item; }
+        }
+
+        public double ItemWeight
+        {
+            set { this.itemWeight = value; }
+            get { return this.itemWeight; }
+        }
+        public double GetItemList()
+        {
+            
+            List<double> items = new List<double>();
+            items.Add(this.item);
+            double getItem = 0;
+            for (var i = 0; i<items.Count; i++)
+            {
+                  getItem = items[i];
+                
+            }
+            return getItem;
+        }
+        public double GetItemWeightList()
+        {
+            List<double> itemWeights = new List<double>();
+            itemWeights.Add(this.itemWeight);
+            double getWeight = 0;
+            for(var i =0; i < itemWeights.Count; i++)
+            {
+                getWeight = itemWeights[i];
+            }
+            return getWeight;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double currentWeight = this.item * this.itemWeight;
+            return currentWeight;
+            
+        }
+        public double GetRemainingWeight()
+        {
+            double remainingWeight = this.maximumWeight - this.GetCurrentWeight();
+            return remainingWeight;
+        }
+
+
+    }
+}
---
+++ RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        Refrigerator refrigerator = new Refrigerator();
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            refrigerator.MaxiMumWeight = Convert.ToDouble(maxWeightTakeTextBox.Text);
+
+            //refrigerator = new Refrigerator(Convert.ToDouble(maxWeightTakeTextBox.Text));
+            maxWeightTakeTextBox.Clear();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            refrigerator.Items = Convert.ToDouble(itemTextBox.Text);
+            refrigerator.ItemWeight = Convert.ToDouble(weightTextBox.Text);
+
+            //refrigerator.GetItem(Convert.ToDouble(itemTextBox.Text));
+            //refrigerator.GetItemWeight(Convert.ToDouble(weightTextBox.Text));
+            itemTextBox.Clear();
+            weightTextBox.Clear();
+            if(refrigerator.GetCurrentWeight() > refrigerator.MaxiMumWeight)
+            {
+                MessageBox.Show("CurrentWeight is OverFlow the maximum weight");
+            }
+            else
+            {
+                currentWeightTextBox.Text = refrigerator.GetCurrentWeight().ToString();
+                remainingWeightTextBox.Text = refrigerator.GetRemainingWeight().ToString();
+
+                richTextBox.Text +=  refrigerator.GetItemList().ToString() + ",  "+
+                                    refrigerator.GetItemWeightList().ToString() + ",  " +
+                                    refrigerator.GetCurrentWeight().ToString()+"\n";
+            }
+
+
+        }
+    }
+}
